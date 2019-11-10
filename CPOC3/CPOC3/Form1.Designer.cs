712db962a9f4b6bea65eaf2db413a2ea848886ad
CookieAmbassador <47668041+CookieAmbassador@users.noreply.github.com>
2019-11-10 15:19:53 +0200

    Add files via upload

---
+++ CPOC3/CPOC3/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace CPOC3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(624, 436);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(234, 45);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Array on matrix";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(624, 538);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(234, 45);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Shut down";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(606, 259);
+            this.richTextBox2.TabIndex = 7;
+            this.richTextBox2.Text = "";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(624, 487);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(234, 45);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Matrix on array";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 326);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(606, 249);
+            this.richTextBox1.TabIndex = 9;
+            this.richTextBox1.Text = "";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(624, 385);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(234, 45);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Fill files";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(635, 326);
+            this.progressBar2.Maximum = 20;
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(223, 53);
+            this.progressBar2.TabIndex = 11;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(635, 12);
+            this.progressBar1.Maximum = 20;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(223, 53);
+            this.progressBar1.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(880, 587);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ CPOC3/CPOC3/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+using System.IO;
+
+namespace CPOC3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int size = 1000;
+        Random rnd = new Random();
+        Random rnd1 = new Random();
+
+        void get_column(int[] ar, int ind)
+        {
+            StreamReader str1 = new StreamReader("matrix.txt");
+            BinaryReader binr1 = new BinaryReader(str1.BaseStream);
+            binr1.BaseStream.Position = ind*sizeof(Int32);
+            for (int i=0;i<size;i++)
+            {
+                ar[i] = binr1.ReadInt32();
+                binr1.BaseStream.Seek((size-1)*sizeof(Int32), SeekOrigin.Current);
+            }
+            binr1.Close();
+            binr1.Dispose();
+            str1.DiscardBufferedData();
+            str1.Close(); 
+        }
+
+        void get_row(int[] ar, int ind)
+        {
+            StreamReader str1 = new StreamReader("matrix.txt");
+            BinaryReader binr1 = new BinaryReader(str1.BaseStream);
+            binr1.BaseStream.Position = ind * sizeof(int) * size; ;
+            for (int i = 0; i < size; i++)
+            {
+                ar[i] = binr1.ReadInt32();
+            }
+            binr1.Close();
+            binr1.Dispose();
+            str1.DiscardBufferedData();
+            str1.Close();
+        }
+
+        void do_1()
+        {
+            for (int d = 0; d < 20; d++)
+            {
+                Stopwatch StopwatchArray = new Stopwatch();
+                int[] part_of_matrix = new int[size];
+                int[] res1 = new int[size];
+
+
+
+                #region ReadingArray
+                int[] array = new int[size];
+                StreamReader str = new StreamReader("array.txt");
+                BinaryReader binr = new BinaryReader(str.BaseStream);
+                int k = 0;
+                while (binr.PeekChar() > -1)
+                {
+
+                    array[k] = binr.ReadInt32();
+                    k++;
+                }
+                binr.Close();
+                str.DiscardBufferedData();
+                str.Close();
+                #endregion
+
+                StopwatchArray.Start();
+                for (int i = 0; i < size; i++)
+                {
+                    res1[i] = 0;
+                    get_column(part_of_matrix, i);
+                    for (int j = 0; j < size; j++)
+                    {
+                        res1[i] += array[j] * part_of_matrix[j];
+                    }
+                    Array.Clear(part_of_matrix, 0, size);
+                }
+                StopwatchArray.Stop();
+                TimeSpan time1 = StopwatchArray.Elapsed;
+                StopwatchArray.Reset();
+                string elapsedTime1 = Convert.ToString(time1.Milliseconds);
+                Action action = () => richTextBox2.Text += ("" + ": time for array on matrix: " + elapsedTime1 + Environment.NewLine + "");
+                if (InvokeRequired)
+                    Invoke(action);
+                else
+                    action();
+                Array.Clear(res1, 0, size);
+                Action action2 = () => progressBar1.Value++;
+                if (InvokeRequired)
+                    Invoke(action2);
+                else
+                    action2();
+            }
+            
+        }
+
+        void do_2()
+        {
+            for (int d = 0; d < 20; d++)
+            {
+                Stopwatch StopwatchMatrix = new Stopwatch();
+                int[] part_of_matrix = new int[size];
+                int[] res2 = new int[size];
+
+
+                #region ReadingArray
+                int[] array = new int[size];
+                StreamReader str = new StreamReader("array.txt");
+                BinaryReader binr = new BinaryReader(str.BaseStream);
+                int k = 0;
+                while (binr.PeekChar() > -1)
+                {
+
+                    array[k] = binr.ReadInt32();
+                    k++;
+                }
+                binr.Close();
+                str.DiscardBufferedData();
+                str.Close();
+                #endregion
+
+                StopwatchMatrix.Start();
+                {
+                    for (int i = 0; i < size; i++)
+                    {
+                        res2[i] = 0;
+                        get_row(part_of_matrix, i);
+                        for (int j = 0; j < size; j++)
+                        {
+                            res2[i] += part_of_matrix[j] * array[j];
+                        }
+                        Array.Clear(part_of_matrix, 0, size);
+                    }
+                }
+                StopwatchMatrix.Stop();
+                TimeSpan time2 = StopwatchMatrix.Elapsed;
+                StopwatchMatrix.Reset();
+                string elapsedTime2 = Convert.ToString(time2.Milliseconds);
+                Action action = () => richTextBox1.Text += ("" + ": time for matrix on array: " + elapsedTime2 + Environment.NewLine + "");
+                if (InvokeRequired)
+                    Invoke(action);
+                else
+                    action();
+                Array.Clear(res2, 0, size);
+                Action action2 = () => progressBar2.Value++;
+                if (InvokeRequired)
+                    Invoke(action2);
+                else
+                    action2();
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Thread mult = new Thread(do_1);
+            mult.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Thread mult1 = new Thread(do_2);
+            mult1.Start();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            #region Filling
+            int[] arraywrite = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                arraywrite[i] = rnd.Next() % 3 - 1;
+            }
+            int[,] matrix = new int[size, size];
+            StreamWriter streamWriter = new StreamWriter("array.txt");
+            BinaryWriter binaryWriter = new BinaryWriter(streamWriter.BaseStream);
+            for (int i = 0; i < size; i++)
+            {
+                binaryWriter.Write(arraywrite[i]);
+            }
+            binaryWriter.Close();
+            BinaryWriter fs_matrix = new BinaryWriter(File.Open("matrix.txt", FileMode.Open));
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    matrix[i, j] = rnd1.Next() % 3 - 1;
+                    fs_matrix.Write(matrix[i, j]);
+                }
+            }
+            fs_matrix.Close();
+            #endregion
+        }
+    }
+}
